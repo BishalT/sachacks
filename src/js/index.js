ce6dd21b249fc8ed5c42f8abf8c1711e990fc378
Lambert Tran <lb.tran648@gmail.com>
2018-02-19 22:43:05 -0800

    added about-schedule page

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,22 @@
+// javascript helpers
+import NavBar from './utils/navbar';
+
+// scss
 import '../scss/index.scss';
+
+
 let allSlides = document.querySelectorAll('.page');
 let controllBtns = document.querySelectorAll('.control-btn li');
 
-
+const navbar = new NavBar(allSlides,controllBtns);
 
 // initiate the sliders
-startFirstSlide.apply(allSlides)
+navbar.startFirstSlide();
 
 // add click event all all control btn
 controllBtns.forEach((btn) => {
-  btn.addEventListener('click',handleSlideMove);
+  btn.addEventListener('click',function(){
+    navbar.handleSlideMove(this);
+  });
 })
 
-function handleSlideMove() {
-  let target = this.getAttribute('name');
-  // remove and reset everything
-  resetAllSlides.apply(allSlides);
-  removeAllActiveClass.apply(controllBtns);
-  // display right image and add active class to btn
-  allSlides[target].style.display = 'block';
-  addActiveStyle.apply(this);
-}
-
-function startFirstSlide() {
-  resetAllSlides.apply(this);
-  this[0].style.display = 'block';
-  addActiveStyle.apply(controllBtns[0]);
-}
-
-function resetAllSlides() {
-  this.forEach((slide) => {
-    slide.style.display = 'none';
-  })
-}
-
-function addActiveStyle() {
-  this.classList.add('active');
-}
-
-function removeAllActiveClass() {
-  this.forEach((btn) => {
-    btn.classList.remove('active');
-  })
-}
