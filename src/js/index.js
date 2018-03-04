13719b5af1dad33e9d74689b53b70111ffe223a1
Lambert Tran <lb.tran648@gmail.com>
2018-03-03 17:52:24 -0800

    made changes on mobile and desktop version, add some animation on page and navbar

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import NavBar from './utils/navbar';
 // import switchPageOnWheel from './utils/switchPageOnWheel';
 
 // scss
+import 'animate.css';
 import '../scss/index.scss';
 
 // search the DOM
@@ -13,23 +14,29 @@ const controllBtns = document.querySelectorAll('.control-btn li');
 const rocketParent = document.querySelector('.rocket');
 
 // instantiate navbar
-const navbar = new NavBar(pages,controllBtns,rocketParent);
+const navbar = new NavBar(pages,controllBtns,rocketParent,'fadeInUp','fadeInDown');
+
+// set current page
 let pageNum = 0;
 
-
+// add animation
+pages.forEach((pg) => {
+  pg.classList.add('animated');
+})
 
 // change background of wrapper - need to use JS because github add a '/' to the end point of url -> cant access background image
 // EX: github/sachacks/ 
 changeBgImg.target(wrapper);
 
+// activate navbar whenever screen is bigger than 1023px
 if (window.innerWidth > 1023) {
   // initiate homepage
   navbar.init();
 }
-
 window.addEventListener('resize', () => {
   if (window.innerWidth > 1023) {
     // initiate homepage
     navbar.init();
   }
-})
+});
+
--- src/js/utils/navbar.js
+++ src/js/utils/navbar.js
@@ -2,10 +2,13 @@
 // It will set all pages, but homepage, to display none
 
 class NavBar {
-  constructor(pages,controllBtns,rocketParent) {
+  constructor(pages,controllBtns,rocketParent,animatedUp,animatedDown) {
     this.pages = pages;
     this.controllBtns = controllBtns;
     this.rocketParent = rocketParent;
+    this.animatedUp = animatedUp;
+    this.animatedDown = animatedDown;
+    this.currPage = 0;
   }
 
   // initiating first page
@@ -14,25 +17,52 @@ class NavBar {
     switchPage.apply(this.pages[0]);          // switch to first page
     addActiveBtn.apply(this.controllBtns[0]); // add active class to first page
     initControllBtn.apply(this);
+    moveRocket.call(this,this.controllBtns[0]);   // move rocket to first button
   }
 
   // switching page when navbar btn is clicked
   handleBtnClick(btn) {
     let target = btn.getAttribute('name');    // get the index of btn
-    moveRocket.call(this,btn);                // move rocket to btn
     resetAllPages.apply(this.pages);          // reset pages
     removeActiveBtn.apply(this.controllBtns); // remove style on btn
+    removeAnimation.apply(this);              // remove all animation effect
+    moveRocket.call(this,btn);                // move rocket to btn
+    changeAnimation.call(this,target);        // animate the target page
     switchPage.apply(this.pages[target])      // switch to page clicked
     addActiveBtn.apply(btn);                  // add style to btn
   }
 }
 
+function removeAnimation() {
+  this.pages.forEach((pg) => {
+    pg.classList.remove(`${this.animatedDown}`)
+    pg.classList.remove(`${this.animatedUp}`)
+  })
+}
+
+function changeAnimation(target) {
+  if (target > this.currPage) {
+    this.pages[target].classList.add(`${this.animatedUp}`);
+  } else {
+    this.pages[target].classList.add(`${this.animatedDown}`);
+  }
+  this.currPage = target;
+}
+
 function moveRocket(btn) {
-  let dist = getPosBtn.call(this,btn) - 30; // minus 1/2 of rocket
+  let dist = getPosBtn.call(this,btn) - 30; // minus 1/2 of rocket height
   const elements = this.rocketParent.children;
   for (let i=0; i < elements.length; ++i) {
     if (elements[i].tagName === 'IMG'){
-      elements[i].style.marginTop = `${dist}px`;
+      let rocket = elements[i];
+      // if it goes down, change direction of 
+      if (btn.getAttribute('name') >= this.currPage) {  
+        rocket.style.transform = 'rotate(180deg)';
+          
+      } else {
+        rocket.style.transform = 'rotate(0deg)';        
+      }
+      rocket.style.marginTop = `${dist}px`;
     }
   } 
   function getPosBtn(btn) {
