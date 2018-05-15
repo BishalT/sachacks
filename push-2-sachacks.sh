83f35f44b77e648b721b5be1e8666b68593cedb9
Lambert Tran <lb.tran648@gmail.com>
2018-05-14 22:55:39 -0700

    added deploy script

---
+++ push-2-sachacks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#### Check if remote to sachack exists
+# get a list of git remote
+REMOTE=$(git remote -v)
+IS_LINKED=false
+
+# loop through item to check if sachacks remote is linked
+for item in $REMOTE
+do 
+  if [ "$item" = "sachacks" ]
+  then
+    IS_LINKED=true
+  fi
+done
+
+# if not, then link it to current git repo
+if [ "IS_LINKED=false"] 
+then 
+  git remote add sachacks git@github-saschacks:sachacks/sachacks.git
+fi
+
+#### Switch git account
+git checkout sachacks/gh-pages
+
+#### Merge with the origin
+git pull https://github.com/LambertTran/sachacks.git gh-pages
+
+#### Push
+git push sachacks gh-pages
+
+#### Switch back to the original account
+git checkout master
