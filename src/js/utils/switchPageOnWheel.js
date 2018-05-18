3970f4494f29a593fec04884fcdcfee9521e00bc
Lambert Tran <lb.tran648@gmail.com>
2018-05-18 15:57:37 -0700

    changed mousewheel to wheel event, allowed user to scroll on both mousewheel and trackpad

--- src/js/utils/switchPageOnWheel.js
+++ src/js/utils/switchPageOnWheel.js
@@ -8,16 +8,50 @@ const switchPageOnWheel = (function() {
     listBtns = btns;
     scroll = false;
     let scrolled = false;
+    let fired = 0;
+    let isWheel;
+
+    // 'wheel' event work for both mousewheel and trackpad
+    window.addEventListener('wheel', function(event) {
+      // This timer is set for trackpad
+      // when user scrolls using trackpad, it will fire wheel event at least 20 times
+      // when user stops scrolling, it will switch page
+      clearTimeout( isWheel );
+
+      fired ++; // count how many event fired
+
+      isWheel = setTimeout(function() {
+        callback(event,fired);
+        fired = 0;
+      },35)
+    });
+
+    function callback(event,fired) {
+      // when user scrolls using trackpad, 'wheels' event usually fires more than 6 times
+      if (fired > 6) {
+        let direction = event.deltaY;
+        let fakeEvent={};
+        if (direction > 0) {
+          fakeEvent.deltaY = 100;
+        }else {
+          fakeEvent.deltaY = -100;
+        }
+        debounce(fakeEvent,handleScrollDelay,200);
+      }
+      // this else statement is used for mousewheel
+      else{
+        debounce(event,handleScrollDelay, 400);
+        fired = 0;
+      }
+
+    }
     // delay created inbetween scroll inputs
     function debounce(event, method, delay) {
-    clearTimeout(method._tId);
-    method._tId= setTimeout(function(){
+      clearTimeout(method._tId);
+      method._tId= setTimeout(function() {
         method(event);
-        }, delay);
+      }, delay);
     }
-    window.addEventListener('mousewheel', function(event) {
-        debounce(event,handleScrollDelay, 400);
-    });
   };
 
   return {
